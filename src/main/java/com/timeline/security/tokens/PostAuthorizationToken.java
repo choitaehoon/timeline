d9f5b1769bfc2c3bd1e530de70ce0af32d59e1f3
choitaehoon <xognsdl1230@naver.com>
2020-03-19 12:56:14 +0900

    security 버전 5.3으로 upgrade 및 AccountContext 모델로 변환시켜주는 메소드

--- src/main/java/com/timeline/security/tokens/PostAuthorizationToken.java
+++ src/main/java/com/timeline/security/tokens/PostAuthorizationToken.java
@@ -1,6 +1,8 @@
 package com.timeline.security.tokens;
 
+import com.timeline.security.AccountContext;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 
 import java.util.Collection;
@@ -9,6 +11,10 @@ public class PostAuthorizationToken extends UsernamePasswordAuthenticationToken 
 
     public PostAuthorizationToken(Object principal, Object credentials, Collection<? extends GrantedAuthority> authorities) {
         super(principal, credentials, authorities);
+    }
+
+    public static Authentication getTokenFromAccountContext(AccountContext accountModel) {
+        return new PostAuthorizationToken(accountModel, accountModel.getPassword(), accountModel.getAuthorities());
     }
 
     public String getUsername() {
